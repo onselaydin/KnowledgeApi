a6fb834b18669e31a77887025b09411474835b22
Önsel AYDIN <onselaydin@gmail.com>
2021-02-02 11:20:43 +0300

    backend modify

--- Models/ArtType.cs
+++ Models/ArtType.cs
@@ -6,11 +6,11 @@ namespace KnowledgeApi.Models
     public class ArtType: MongoBaseModel
     {
         [BsonElement("title")]
-        [Required(ErrorMessage = "Başlık alanı boş geçilemez.")]
+        //[Required(ErrorMessage = "Başlık alanı boş geçilemez.")]
         public string Title { get; set; }
 
         [BsonElement("description")]
-        [Required(ErrorMessage = "Açıklama alanı boş geçilemez.")]
+        //[Required(ErrorMessage = "Açıklama alanı boş geçilemez.")]
         public string Description { get; set; }
 
         [BsonElement("dates")]
--- Models/Article.cs
+++ Models/Article.cs
@@ -7,29 +7,29 @@ namespace KnowledgeApi.Models
     public class Article : MongoBaseModel
     {
         [BsonElement("title")]
-        [Required(ErrorMessage = "Başlık alanı boş geçilemez.")]
+        //[Required(ErrorMessage = "Başlık alanı boş geçilemez.")]
         public string Title { get; set; }
 
         [BsonElement("content")]
-        [Required(ErrorMessage = "İçerik alanı boş geçilemez.")]
+        //[Required(ErrorMessage = "İçerik alanı boş geçilemez.")]
         public string Content { get; set; }
 
         [BsonElement("description")]
-        [Required(ErrorMessage = "Açıklama alanı boş geçilemez.")]
+        //[Required(ErrorMessage = "Açıklama alanı boş geçilemez.")]
         public string Description { get; set; }
 
         [BsonElement("topics")]
         public string Topics { get; set; }
 
         [BsonElement("url")]
-        [Required(ErrorMessage = "Url alanı boş geçilemez.")]
+        //[Required(ErrorMessage = "Url alanı boş geçilemez.")]
         public string Url { get; set; }
 
         [BsonElement("articletype")]
         public string ArticleType { get; set; }
 
         [BsonElement("image")]
-        [Required(ErrorMessage = "Resim alanı boş geçilemez.")]
+        //[Required(ErrorMessage = "Resim alanı boş geçilemez.")]
         public string Image{ get; set; }
 
         [BsonElement("dates")]
